d64bafee0182e67a6dacc7868b1116b0d20ece94
Jörn Schumacher <joern.schumacher@cern.ch>
2015-10-06 14:04:20 +0200

    Add event debug threads
    Added threads to ibmsg-recv, ibmsg-send to print occuring events to
    stdout. This is useful for debugging.

--- ibmsg-send.c
+++ ibmsg-send.c
@@ -11,12 +11,27 @@ Author: Jörn Schumacher <joern.schumacher@cern.ch>
 
 #define APPLICATION_NAME "ibmsg-send"
 
-
 #define TRACE(x)   \
 do { int result = x;    \
 if(verbose) fprintf(stdout, "%s -> %d\n", #x, result); \
 else if(0 != result) fprintf(stderr, APPLICATION_NAME": error: '%s' failed, error=%d\n", #x, result); \
 } while(0)
+
+
+static void*
+thread_print_events(void* context)
+{
+	struct ibv_async_event event;
+
+	while(1)
+	{
+		if(0 != ibv_get_async_event((struct ibv_context*)context, &event))
+			return NULL;
+		printf("EVENT TRACE: %s\n", ibv_event_type_str(event.event_type));
+		ibv_ack_async_event(&event);
+	}
+	return NULL;
+}
 
 int
 main(int argc, char** argv)
@@ -26,6 +41,7 @@ main(int argc, char** argv)
 	ibmsg_address addr;
 	ibmsg_message msg;
 	uint64_t handle;
+	pthread_t event_thread;
 
 	char* dev;
 	uint8_t port;
@@ -44,8 +60,16 @@ main(int argc, char** argv)
 	addr.is_global = 0;
 
 	TRACE( ibmsg_open(&conn, dev, port) );
+
+	if(0 != pthread_create(&event_thread, NULL,
+                          thread_print_events, (void*)conn.context))
+	{
+		fprintf(stderr, "Could not create event thread\n");
+		exit(EXIT_FAILURE);
+	}
+
 	TRACE( ibmsg_connect(&conn, addr) );
-	TRACE( ibmsg_alloc(&conn, &msg, 4096) );
+	TRACE( ibmsg_alloc(&conn, &msg, 512) );
 
 	TRACE( ibmsg_send(&conn, &msg, &handle) );
 	TRACE( ibmsg_wait_send(&conn) );
@@ -53,6 +77,8 @@ main(int argc, char** argv)
 	TRACE( ibmsg_free(&msg) );
 	TRACE( ibmsg_disconnect(&conn) );
 	TRACE( ibmsg_close(&conn) );
+
+	pthread_join(event_thread, NULL);
 
 	return EXIT_SUCCESS;
 }
