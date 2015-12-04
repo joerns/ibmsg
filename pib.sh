56a053c07729b1fa7ef6f340b09332e2e09ffa3f
Jörn Schumacher <joern.schumacher@cern.ch>
2015-12-04 10:39:57 +0100

    Add IP configuration to PIB control script
    The script pib.sh, used to start and stop the PIB service for testing,
    now also configures IP addresses on the virtual Infiniband links for
    IPoIB. It uses the addresses 192.168.100.100-103.

--- pib.sh
+++ pib.sh
@@ -14,6 +14,17 @@ if [[ "$1" == "start" ]]; then
 	service rdma start
 	modprobe pib
 	service opensm start
+
+    ip addr add 192.168.100.100/24 dev ib0                                                                                                                                                                                                         
+    ip addr add 192.168.100.101/24 dev ib1
+    ip addr add 192.168.100.102/24 dev ib2
+    ip addr add 192.168.100.103/24 dev ib3
+
+    ip link set ib0 up
+    ip link set ib1 up
+    ip link set ib2 up
+    ip link set ib3 up
+
 elif [[ "$1" == "stop" ]]; then
 	service opensm stop
 	rmmod pib
